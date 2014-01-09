bab455b606df15c887e2a41e5e9eef86904ca5f5
Christophe Pythoud <git@pythoud.net>
2014-01-08 21:31:03 -0700

    Initial setup.

--- src/org/dbbeans/sql/DBQuerySetupRetrieveData.java
+++
@@ -1,9 +0,0 @@
-package org.dbbeans.sql;
-
-/**
- * In conjunction with the {@link DBAccess} or {@link DBTransaction} class, use implementations of this interface to insert the code for setting up
- * a database query and process its results.
- *
- * You should create an anonymous inner class (Java) or a closure (Groovy).
- */
-public interface DBQuerySetupRetrieveData<T> extends DBQuerySetup, DBQueryRetrieveData<T> { }
