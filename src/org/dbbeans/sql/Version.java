1bebfd9b2f30dba59c87bfa94103553dbbfcd99c
chris <git@pythoud.net>
2021-09-08 17:13:01 -0500

    Start work on version 0.9.1

--- src/org/dbbeans/sql/Version.java
+++ src/org/dbbeans/sql/Version.java
@@ -8,8 +8,8 @@ public class Version {
     private static final String  PREFIX  = "";
     private static final int     MAJOR   = 0;
     private static final int     MINOR   = 9;
-    private static final int     PATCH   = 0;
-    private static final String  POSTFIX = "-beta";
+    private static final int     PATCH   = 1;
+    private static final String  POSTFIX = "-RC";
 
     private static final String VERSION = PREFIX + MAJOR + "." + MINOR + "." + PATCH + POSTFIX;
 
